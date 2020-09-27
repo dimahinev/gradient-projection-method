998d9fa263b40b77acf27cb0fafae1b605eefc22
Dima Hinev <dh@MacBook-Pro.local>
2020-09-27 08:39:17 +0300

    Updates

---
+++ precache-manifest.8a94512e2cda75b11838d58ddec7c992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188cc7dc5bbe1d8c229d3dacac080a0d",
+    "url": "/gradient-projection-method/index.html"
+  },
+  {
+    "revision": "96d9028467beb825853d",
+    "url": "/gradient-projection-method/static/css/main.0311be47.chunk.css"
+  },
+  {
+    "revision": "44259a7d1d832e912049",
+    "url": "/gradient-projection-method/static/js/2.07da7967.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/gradient-projection-method/static/js/2.07da7967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d9028467beb825853d",
+    "url": "/gradient-projection-method/static/js/main.0f090e4f.chunk.js"
+  },
+  {
+    "revision": "67def6a2ea9961571e20",
+    "url": "/gradient-projection-method/static/js/runtime-main.388ce34a.js"
+  }
+]);
